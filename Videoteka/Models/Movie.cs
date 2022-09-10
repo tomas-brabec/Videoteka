b4b6d49fb49d6854ac8029b9cb7ecc530b589a59
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 14:24:57 +0200

    Add Movie model

---
+++ Videoteka/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Videoteka.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; }
+    }
+}
