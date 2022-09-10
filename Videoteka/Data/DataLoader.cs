33a8afb5837cd41ae2e95466e6eac36cb2b4fb52
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 17:52:32 +0200

    Add more records to genres and movies

--- Videoteka/Data/DataLoader.cs
+++ Videoteka/Data/DataLoader.cs
@@ -18,12 +18,19 @@ namespace Videoteka.Data
             }
         }
 
-        private static void LoadGenres(MoviesDbContext context) 
+        private static void LoadGenres(MoviesDbContext context)
         {
             context.Genres.AddRange(
-                new Genre() { Name = "SciFi" },
-                new Genre() { Name = "Krimi" }
-                );
+                new Genre() { Name = "Drama" },
+                new Genre() { Name = "Komedie" },
+                new Genre() { Name = "Dobrodružný" },
+                new Genre() { Name = "Western" },
+                new Genre() { Name = "Historický" },
+                new Genre() { Name = "Životopisný" },
+                new Genre() { Name = "Sci-Fi" },
+                new Genre() { Name = "Horor" },
+                new Genre() { Name = "Akční" }
+            );
 
             context.SaveChanges();
         }
@@ -31,7 +38,10 @@ namespace Videoteka.Data
         private static void LoadMovies(MoviesDbContext context)
         {
             context.Movies.AddRange(
-                new Movie() { Title = "Alien", ReleaseYear = 1979, GenreId = 1, Rating = 5 }
+                new Movie() { Title = "Vetřelec", ReleaseYear = 1979, GenreId = 6, Rating = 5 },
+                new Movie() { Title = "Blade Runner 2049 ", ReleaseYear = 2017, GenreId = 6, Rating = 4 },
+                new Movie() { Title = "John Wick", ReleaseYear = 2014, GenreId = 9, Rating = 5 },
+                new Movie() { Title = " Babovřesky", ReleaseYear = 2013, GenreId = 2, Rating = 0 }
                 );
 
             context.SaveChanges();
