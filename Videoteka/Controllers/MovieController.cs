a570e86594b8004b6afa74b432d1e601564e2d33
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 15:08:20 +0200

    Add controller method a view for display list of films

--- Videoteka/Controllers/MovieController.cs
+++ Videoteka/Controllers/MovieController.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Videoteka.Data;
 
 namespace Videoteka.Controllers
 {
     public class MovieController : Controller
     {
+        private readonly MoviesDbContext _context;
 
+        public MovieController(MoviesDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index() 
+        {
+            return View(await _context.Movies.Include(m => m.Genre).ToListAsync());
+        }
     }
 }
