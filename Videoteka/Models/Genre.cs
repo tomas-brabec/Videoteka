ff411306a7639cb5b94b56db40d17554fb0834a3
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 14:42:49 +0200

    Add data annotations in Movie model

--- Videoteka/Models/Genre.cs
+++ Videoteka/Models/Genre.cs
@@ -1,8 +1,11 @@
-﻿namespace Videoteka.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Videoteka.Models
 {
     public class Genre
     {
         public int Id { get; set; }
+        [MaxLength(255)]
         public string Name { get; set; } = null!;
     }
 }
--- Videoteka/Models/Movie.cs
+++ Videoteka/Models/Movie.cs
@@ -1,12 +1,18 @@
-﻿namespace Videoteka.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Videoteka.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
         public string Title { get; set; } = null!;
+        [Range(1, 9999)]
         public int ReleaseYear { get; set; }
         public int GenreId { get; set; }
         public Genre Genre { get; set; } = null!;
+        [Range(0, 5)]
         public int Rating { get; set; }
     }
 }
