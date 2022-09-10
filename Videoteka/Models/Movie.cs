7fa3e1158bab6c52d4d4a5437e431bd679097719
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 14:35:23 +0200

    Add separate class for genre

--- Videoteka/Models/Movie.cs
+++ Videoteka/Models/Movie.cs
@@ -3,9 +3,10 @@
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
+        public string Title { get; set; } = null!;
         public int ReleaseYear { get; set; }
-        public string Genre { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; } = null!;
         public int Rating { get; set; }
     }
 }
