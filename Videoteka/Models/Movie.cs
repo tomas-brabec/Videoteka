b92c4e62d46d288e1a51dc154d178e4c7cc67480
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-10 17:27:21 +0200

    GUI improvements

--- Videoteka/Models/Movie.cs
+++ Videoteka/Models/Movie.cs
@@ -5,14 +5,19 @@ namespace Videoteka.Models
     public class Movie
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "{0} je povinný údaj")]
         [MaxLength(255)]
+        [Display(Name = "Název")]
         public string Title { get; set; } = null!;
-        [Range(1, 9999)]
+        [Required(ErrorMessage = "{0} je povinný údaj")]
+        [Range(1, 9999, ErrorMessage = "Zadejte pouze rok v rozmezí 1 až 9999")]
+        [Display(Name = "Rok vydání")]
         public int ReleaseYear { get; set; }
         public int GenreId { get; set; }
+        [Display(Name = "Žánr")]
         public Genre? Genre { get; set; } = null!;
-        [Range(0, 5)]
+        [Range(0, 5, ErrorMessage = "Zadejte hodnotu v rozmezí 0 až 5")]
+        [Display(Name = "Hodnocení")]
         public int Rating { get; set; }
     }
 }
