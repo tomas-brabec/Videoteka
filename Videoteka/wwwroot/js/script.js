fc7167dfc695ef7667a53021ed876867474bfe95
Tomáš Brabec <tomas.brabec.98@outlook.com>
2022-09-26 14:11:45 +0200

    remove unnecessary hiddent input used for Rating

--- Videoteka/wwwroot/js/script.js
+++ Videoteka/wwwroot/js/script.js
@@ -1,6 +1,4 @@
 ﻿function inputChange(i) {
-    let hiddenInput = document.getElementById("Rating");
     let label = document.getElementById("labelRating");
-    hiddenInput.value = i;
     label.innerText = `Hodnocení: ${i}/5`;
 }
